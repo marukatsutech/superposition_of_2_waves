91ea5e6e5d05aa20a14778fe051e4d119fad60b9
marukatsutech <marukatsutech@gmail.com>
2021-10-31 13:34:05 +0800

    Correct omega(no multiply pi), change labels etc

--- superposition_of_2_waves_2d.py
+++ superposition_of_2_waves_2d.py
@@ -38,33 +38,33 @@ def change_a2(value):
 
 def reset_parameter():
     global k1, k2, omega1, omega2, a1, a2
-    k1 = 1.
-    k2 = 1.
-    omega1 = omega_max
-    omega2 = omega_min
+    k1 = k_initial
+    k2 = k_initial
+    omega1 = omega_initial
+    omega2 = - omega_initial
     a1 = a_max
     a2 = a_max
     # Set Initial value of spinbox
     var_a1.set(a1)
     var_k1.set(k1)
-    var_o1.set(omega1)
+    var_o1.set(omega_initial)
     var_a2.set(a2)
     var_k2.set(k2)
-    var_o2.set(omega2)
+    var_o2.set(- omega_initial)
 
 
 def set_axis():
     ax1.set_xlim(x_min, x_max)
     ax1.set_ylim(y_min, y_max)
-    ax1.set_title('Sine waves; A * sin(k*x -omega*t)')
-    ax1.set_ylabel('y')
+    ax1.set_title('Sine waves; A * sin(k*x - omega*t)')
+    ax1.set_ylabel('y; Amplitude')
     ax1.grid()
 
     ax2.set_xlim(x_min, x_max)
     ax2.set_ylim(y_min, y_max)
     ax2.set_title('Superposed wave')
-    ax2.set_xlabel('x (* pi)')
-    ax2.set_ylabel('y')
+    ax2.set_xlabel('x; Phase (* pi)')
+    ax2.set_ylabel('y; Amplitude')
     ax2.grid()
 
 
@@ -73,22 +73,22 @@ def update(f):
     ax2.cla()  # Clear ax
     set_axis()
 
-    ax1.text(x_min, y_max * 0.9, "Step(as t)=" + str(f))
+    ax1.text(x_min, y_max * 0.9, " Step(as t)=" + str(f))
     if k1 != 0:
         vp1 = omega1 / k1
-        ax1.text(x_min, y_max * 0.7, "Phase velocity1(omega1/k1)=" + str(vp1))
+        ax1.text(x_min, y_max * 0.7, " Phase velocity1(omega1/k1)=" + str(vp1))
     if k2 != 0:
         vp2 = omega2 / k2
-        ax1.text(x_min, y_max * 0.5, "Phase velocity2(omega2/k2)=" + str(vp2))
+        ax1.text(x_min, y_max * 0.5, " Phase velocity2(omega2/k2)=" + str(vp2))
     if (k1 - k2) != 0:
         vg = (omega1 - omega2) / (k1 - k2)
-        ax2.text(x_min, y_max * 0.9, "Group velocity(d_omega/dk)=(omega1-omega2)/(k1-k2)=" + str(vg))
+        ax2.text(x_min, y_max * 0.9, " Group velocity(d_omega/dk)=(omega1-omega2)/(k1-k2)=" + str(vg))
     else:
-        ax2.text(x_min, y_max * 0.9, "None group velocity")
+        ax2.text(x_min, y_max * 0.9, " None group velocity")
 
     # Draw sin curve
-    y1 = a1 * np.sin((k1 * x - omega1 * f) * np.pi)  # sin(kx - omega*t)
-    y2 = a2 * np.sin((k2 * x - omega2 * f) * np.pi)  # sin(kx - omega*t)
+    y1 = a1 * np.sin((k1 * x) * np.pi - omega1 * f)  # sin(kx - omega*t)
+    y2 = a2 * np.sin((k2 * x) * np.pi - omega2 * f)  # sin(kx - omega*t)
     y_superposed = y1 + y2
     ax1.plot(x, y1, linestyle='-', label="A1=" + str(a1) + ", k1=" + str(k1) + ", omega1=" + str(omega1))
     ax1.plot(x, y2, linestyle='-', label="A2=" + str(a2) + ", k2=" + str(k2) + ", omega2=" + str(omega2))
@@ -108,11 +108,13 @@ k2 = 1.
 k_min = 0.
 k_max = 20.
 k_step = 1.
-omega1 = 0.25
-omega2 = -0.25
-omega_min = -0.25
-omega_max = 0.25
+omega1 = 1.5
+omega2 = -1.5
+omega_min = -2.0
+omega_max = 2.0
 omega_step = 0.01
+omega_initial = 1.
+k_initial = 1.
 a1 = 1.
 a2 = 1.
 a_min = 0.
@@ -120,7 +122,7 @@ a_max = 1.
 a_step = 0.1
 
 # Generate line space
-x = np.linspace(0, x_max, 1000)
+x = np.linspace(0, x_max, 2000)
 
 # Generate tkinter
 root = tkinter.Tk()
@@ -156,7 +158,7 @@ s_a1.pack(side='left')
 label_k1 = tkinter.Label(root, text="k1")
 label_k1.pack(side='left')
 var_k1 = tkinter.StringVar(root)  # variable for spinbox-value
-var_k1.set(k1)  # Initial value
+var_k1.set(k_initial)  # Initial value
 s_k1 = tkinter.Spinbox(
     root, textvariable=var_k1, format="%.1f", from_=k_min, to=k_max, increment=k_step,
     command=lambda: change_k1(var_k1.get()), width=5
@@ -166,7 +168,7 @@ s_k1.pack(side='left')
 label_o1 = tkinter.Label(root, text="omega1")
 label_o1.pack(side='left')
 var_o1 = tkinter.StringVar(root)  # variable for spinbox-value
-var_o1.set(omega1)  # Initial value
+var_o1.set(omega_initial)  # Initial value
 s_o1 = tkinter.Spinbox(
     root, textvariable=var_o1, format="%.2f", from_=omega_min, to=omega_max, increment=omega_step,
     command=lambda: change_o1(var_o1.get()), width=5
@@ -187,7 +189,7 @@ s_a2.pack(side='left')
 label_k2 = tkinter.Label(root, text="k2")
 label_k2.pack(side='left')
 var_k2 = tkinter.StringVar(root)  # variable for spinbox-value
-var_k2.set(k1)  # Initial value
+var_k2.set(k_initial)  # Initial value
 s_k2 = tkinter.Spinbox(
     root, textvariable=var_k2, format="%.1f", from_=k_min, to=k_max, increment=k_step,
     command=lambda: change_k2(var_k2.get()), width=5
@@ -197,7 +199,7 @@ s_k2.pack(side='left')
 label_o2 = tkinter.Label(root, text="omega2")
 label_o2.pack(side='left')
 var_o2 = tkinter.StringVar(root)  # variable for spinbox-value
-var_o2.set(omega2)  # Initial value
+var_o2.set(- omega_initial)  # Initial value
 s_o2 = tkinter.Spinbox(
     root, textvariable=var_o2, format="%.2f", from_=omega_min, to=omega_max, increment=omega_step,
     command=lambda: change_o2(var_o2.get()), width=5
